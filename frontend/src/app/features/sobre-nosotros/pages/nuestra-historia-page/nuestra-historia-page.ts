f673c711307c6e943fdab143a4e54075cac2c60d
Eduardo <eduardoarevaloportero@gmail.com>
2025-10-20 11:37:55 +0200

    feat: ""Sobe nosotros" y "Nuestra historia"

---
+++ frontend/src/app/features/sobre-nosotros/pages/nuestra-historia-page/nuestra-historia-page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-nuestra-historia-page',
+  imports: [RouterLink],
+  templateUrl: './nuestra-historia-page.html',
+  styleUrl: './nuestra-historia-page.scss'
+})
+export class NuestraHistoriaPage {
+
+}
