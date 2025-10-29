6db0e355100a122d5ee94afd6f374d9a7f3b5539
Eduardo <eduardoarevaloportero@gmail.com>
2025-10-29 12:38:20 +0100

    fix: ajuste de los eventos

--- frontend/src/app/features/eventos/pages/evento-detalle-page/evento-detalle-page.ts
+++ frontend/src/app/features/eventos/pages/evento-detalle-page/evento-detalle-page.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterModule, Router } from '@angular/router';
 
+import { EVENTOS } from '../../data/eventos.data';
+
+
 @Component({
   selector: 'app-evento-detalle-page',
   standalone: true,
@@ -13,62 +16,13 @@ export class EventoDetallePage {
   id!: number;
   evento: any;
 
-  eventos = [
-    {
-      titulo: 'Santo Tardeo',
-      descripcion: '¡Ven disfrazado y sigue la fiesta entre vivos y muertos!',
-      fechas: ['2025-11-1'],
-      imagen: 'assets/eventos/santo_tardeo2025.jpg',
-      texto: `El 1 de noviembre no te pierdas la mejor tarde post-Halloween con Stanz DJ.
-              Música, ambiente y mucha energía desde las 17:30 h. `,
-      enlaces: [
-        { nombre: 'Instagram', url: 'https://www.instagram.com/casinorockbar/' },
-        { nombre: 'Facebook', url: 'https://www.facebook.com/casinorockbar/' }
-      ]
-    },
-    {
-      titulo: 'Pasaje del Terror',
-      descripcion: 'Actividad organizada por los alumnos del C.E.I.P. Miguel de Cervantes (6º curso)',
-      fechas: ['2025-10-31','2025-11-1'],
-      imagen: 'assets/eventos/Pasaje_terror2025.jpg',
-      texto: `Prepárate para una noche escalofriante llena de sustos, risas y mucha adrenalina.
-              Viernes 31 desde las 18:00 y Sábado 1 desde las 20:00.
-              Entrada: 2 € — ¡Todo lo recaudado se destina al viaje de fin de curso!`,
-      enlaces: [
-        { nombre: 'Instagram', url: 'https://www.instagram.com/casinorockbar/' },
-        { nombre: 'Facebook', url: 'https://www.facebook.com/casinorockbar/' }
-      ]
-    },
-    {
-      titulo: 'Hamburbeza',
-      descripcion: 'Las 8 mejores hamburguesas del mundo.',
-      fechas: ['2025-10-10', '2025-10-12'],
-      imagen: 'assets/eventos/Burguer.jpeg',
-      texto: `Ven y prueba nuestras hamburguesas artesanales, preparadas con ingredientes frescos y música en vivo.`,
-      enlaces: [
-        { nombre: 'Instagram', url: 'https://www.instagram.com/casinorockbar/' },
-        { nombre: 'Facebook', url: 'https://www.facebook.com/casinorockbar/' }
-      ]
-    },
-    {
-      titulo: 'Colegui Fest',
-      descripcion: 'Conciertos en vivo.',
-      fechas: ['2025-10-25'],
-      imagen: 'assets/eventos/Concierto.jpeg',
-      texto: `Disfruta del mejor ambiente rock con bandas locales e invitados especiales. ¡No te lo pierdas!`,
-      enlaces: [
-        { nombre: 'Instagram', url: 'https://www.instagram.com/casinorockbar/' },
-        { nombre: 'Facebook', url: 'https://www.facebook.com/casinorockbar/' }
-      ]
-    }
-  ];
-
   constructor(
     private route: ActivatedRoute,
     private router: Router
   ) {
     this.id = Number(this.route.snapshot.paramMap.get('id'));
-    this.evento = this.eventos[this.id];
+    this.evento = EVENTOS.find((e: any) => e.id === this.id);
+    if (!this.evento) this.router.navigate(['/en-construccion']);
   }
 
 
--- frontend/src/app/features/eventos/pages/eventos-page/eventos-page.ts
+++ frontend/src/app/features/eventos/pages/eventos-page/eventos-page.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventoItem } from '../../components/evento-item/evento-item';
 import { RouterModule } from '@angular/router';
+
+import { EVENTOS, Evento } from '../../data/eventos.data';
 
 
 @Component({
@@ -11,36 +13,7 @@ import { RouterModule } from '@angular/router';
   templateUrl: './eventos-page.html',
   styleUrls: ['./eventos-page.scss']
 })
+
 export class EventosPage {
-  eventos = [
-    {
-      titulo: 'Santo Tardeo',
-      fechas: ['2025-11-1'],
-      imagen: 'assets/eventos/santo_tardeo2025.jpg',
-      activo: true
-    },
-  {
-      titulo: 'Pasaje del Terror',
-      fechas: ['2025-10-31','2025-11-1'],
-      imagen: 'assets/eventos/Pasaje_terror2025.jpg',
-      activo: true
-    },
-    {
-      id: 0,
-      titulo: 'Hamburbeza',
-      descripcion: 'Las 8 mejores hamburgesas del mundo.',
-      fechas: ['2025-10-10', '2025-10-12'],
-      imagen: 'assets/eventos/Burguer.jpeg',
-      activo: true
-    },
-    {
-      titulo: 'Colegui Fest',
-      fechas:[ '2025-10-25'],
-      imagen: 'assets/eventos/Concierto.jpeg',
-      enlace: 'https://www.instagram.com/casinorockbar/?hl=es',
-      activo: true
-    }
-  ];
+  eventos: Evento[] = [...EVENTOS].sort((a, b) => b.id - a.id);
 }
-
-
